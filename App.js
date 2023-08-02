35e32d5b7558935e3f96e7397200875426361aa4
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-08-03 01:03:36 +0530

    tinder clone app

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.card}>
+        <ImageBackground
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG",
+          }}
+          style={styles.image}
+        >
+          <View style={styles.cardtext}>
+            <Text style={styles.name}>Vadim Savin</Text>
+            <Text style={styles.bio}>
+              I willbe the semicolons to your codeI will be the semicolons to
+              your codeI will be the semicolons to your codeI will be the
+              semicolons to your codeI will be the semicolons to your codeI will
+              be the semicolons to your code
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +29,48 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    overflow: "hidden",
+    justifyContent: "flex-end",
+  },
+  card: {
+    width: "95%",
+    height: "70%",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 9,
+  },
+  name: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  bio: {
+    color: "white",
+    fontSize: 18,
+    lineHeight: 28,
+    // fontWeight: "regular",
+  },
+  cardtext: {
+    padding: 10,
+    backgroundColor: "red",
   },
 });
